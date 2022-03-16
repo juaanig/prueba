03f2f19bd61daa26df584b83bbcfd216e7216aaa
juani <juangarnero@hotmail.com.ar>
2022-03-15 22:16:06 -0300

    app index

---
+++ app.js
@@ -0,0 +1,33 @@
+const $container = document.getElementById('container');
+
+
+
+
+var card = `
+        <div class="card">
+            <h2 class="title-card">TITULO</h2>
+            <div class="card-content">
+                <img src="space.jpg" alt="">
+                <p class="p-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quos enim animi id sequi numquam expedita nulla, ut voluptatibus incidunt deleniti laborum maiores, culpa et officiis magnam, reprehenderit sit at.</p>
+            </div>
+        </div>
+        <div class="card">
+            <h2 class="title-card">TITULO</h2>
+            <div class="card-content">
+                <img src="space.jpg" alt="">
+                <p class="p-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quos enim animi id sequi numquam expedita nulla, ut voluptatibus incidunt deleniti laborum maiores, culpa et officiis magnam, reprehenderit sit at.</p>
+            </div>
+        </div>
+        <div class="card">
+            <h2 class="title-card">TITULO</h2>
+            <div class="card-content">
+                <img src="space.jpg" alt="">
+                <p class="p-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quos enim animi id sequi numquam expedita nulla, ut voluptatibus incidunt deleniti laborum maiores, culpa et officiis magnam, reprehenderit sit at.</p>
+            </div>
+        </div>
+    `
+
+    $container.innerHTML = card;
+
+
+
