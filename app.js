5fc0e47922b50a9717254c9370be410402ebb2f7
juani <juangarnero@hotmail.com.ar>
2022-03-17 00:59:19 -0300

    actualización 2.1

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-const $container = document.getElementById('container');
-
-
-
-
 var card = `
         <div class="card">
             <h2 class="title-card">TITULO</h2>
@@ -10,24 +5,19 @@ var card = `
                 <img src="space.jpg" alt="">
                 <p class="p-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quos enim animi id sequi numquam expedita nulla, ut voluptatibus incidunt deleniti laborum maiores, culpa et officiis magnam, reprehenderit sit at.</p>
             </div>
-        </div>
-        <div class="card">
-            <h2 class="title-card">TITULO</h2>
-            <div class="card-content">
-                <img src="space.jpg" alt="">
-                <p class="p-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quos enim animi id sequi numquam expedita nulla, ut voluptatibus incidunt deleniti laborum maiores, culpa et officiis magnam, reprehenderit sit at.</p>
-            </div>
-        </div>
-        <div class="card">
-            <h2 class="title-card">TITULO</h2>
-            <div class="card-content">
-                <img src="space.jpg" alt="">
-                <p class="p-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quos enim animi id sequi numquam expedita nulla, ut voluptatibus incidunt deleniti laborum maiores, culpa et officiis magnam, reprehenderit sit at.</p>
-            </div>
-        </div>
-    `
+        </div> 
+        `
 
-    $container.innerHTML = card;
+function adder(){
+    for(let i = 0 ;i < 4; i++){
+        const $container = document.getElementById('container');
+        $container.innerHTML += card;
+    }
+}
+
+
+   
+
 
 
 
